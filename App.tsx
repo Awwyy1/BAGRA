c5f3b61086a297f7b38593d7e28a219b3f057f65
Awwyy1 <154536566+Awwyy1@users.noreply.github.com>
2025-12-25 22:24:51 +0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -104,8 +104,8 @@ const App: React.FC = () => {
       </main>
 
       <footer className="fixed bottom-0 left-0 w-full p-8 pointer-events-none z-40 flex justify-between items-end opacity-20 hidden md:flex">
-        <div className="text-[10px] tracking-widest uppercase font-mono">Точность & Наследие</div>
-        <div className="text-[10px] tracking-widest uppercase font-mono">С 2024</div>
+        <div className="text-[10px] tracking-widest uppercase font-mono">Точность & Характер</div>
+        <div className="text-[10px] tracking-widest uppercase font-mono">С 2025</div>
       </footer>
     </div>
   );
